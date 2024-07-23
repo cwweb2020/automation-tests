86e700fc98e7b0275fbb1f2b2c41e09ffb187995
claudio <bmx347@gmail.com>
2024-07-23 15:48:44 -0300

    previamente a agregar dependencia de extent report

--- src/test/java/day42/orangeHRMTTest.java
+++ src/test/java/day42/orangeHRMTTest.java
@@ -1,9 +1,7 @@
 package day42;
 
 import java.time.Duration;
-
 import org.testng.Assert;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,9 +18,7 @@ public class orangeHRMTTest {
     @BeforeClass
     public void setUp() {
         // Configuración del WebDriver y opciones del navegador
-
         ChromeOptions options = new ChromeOptions();
-        // Configuración para limpiar el caché y las cookies
         options.addArguments("--disable-bundled-ppapi-flash");
         options.addArguments("--disable-plugins-discovery");
         options.addArguments("--disable-plugins");
@@ -33,23 +29,29 @@ public class orangeHRMTTest {
         options.addArguments("--disable-plugins");
 
         driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
 
+        // Acceder a la URL
+        driver.get("https://n5challenge.web.app/");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit(); // Cerrar el navegador al finalizar todas las pruebas
+        }
     }
 
     @Test
     public void testCountAndPrintH2Tags() {
         try {
-            // Acceder a la URL
-            driver.get("https://n5challenge.web.app/");
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));// Espera implícita de 10 segundos
-            driver.manage().window().maximize();
-
             // Contar y capturar los elementos <h2>
             System.out.println("verificando la cantidad de tags <h2>");
             int count = driver.findElements(By.tagName("h2")).size();
             System.out.println("Cantidad de tags h2 en la página: " + count);
 
-            // Capturar y imprimir el contenido de los elementos <h3>
+            // Capturar y imprimir el contenido de los elementos <h2>
             System.out.println("imprimiendo contenido de los tags <h2>");
             for (WebElement element : driver.findElements(By.tagName("h2"))) {
                 System.out.println("Contenido de h2: " + element.getText());
@@ -64,32 +66,27 @@ public class orangeHRMTTest {
 
     @Test
     public void testImagePresence() {
+        try {
+            // Locate image
+            WebElement imgElement = driver
+                    .findElement(By.xpath("//section[@class='banner']/descendant::img[@data-testid='img-banner']"));
 
-        // Localizar la imagen dentro del section usando su atributo data-testid
-        // WebElement imgElement = driver.findElement(By.cssSelector("section.banner
-        // img[data-testid='img-banner']"));
+            // Verify is img is present
+            boolean isImageDisplayed = imgElement.isDisplayed();
 
-        WebElement imgElement = driver
-                .findElement(By.xpath("//section[@class='banner']/descendant::img[@data-testid='img-banner']"));
+            System.out.println("verificando si la imagen se encuentra presente...");
 
-        // Verificar si la imagen está presente
-        boolean isImageDisplayed = imgElement.isDisplayed();
+            // Assert para verificar si la imagen está presente
+            Assert.assertTrue(isImageDisplayed, "La imagen no está presente en la página");
 
-        // Assert para verificar si la imagen está presente
-        Assert.assertTrue(isImageDisplayed, "La imagen no está presente en la página");
+            // Imprimir mensajes indicando el resultado de la verificación
+            if (isImageDisplayed) {
+                System.out.println("Verificación: Imagen presente en la página");
+            }
 
-        // Imprimir mensajes indicando el resultado de la verificación
-        if (isImageDisplayed) {
-            System.out.println("Verificación: Imagen presente en la página");
-        } else {
-            System.out.println("Verificación: Imagen NO presente en la página");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    @AfterClass
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit(); // Cerrar el navegador al finalizar todas las pruebas
-        }
-    }
 }
